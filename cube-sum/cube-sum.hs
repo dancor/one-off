6e5f1c6cd139f6da55deb61b6298cdc8d5e98bbd
Daniel Corson <danl@alum.mit.edu>
2016-02-07 21:39:59 -0500

    cube-sum hash

--- cube-sum/cube-sum.hs
+++ cube-sum/cube-sum.hs
@@ -2,6 +2,7 @@
 -- a^3 + b^3 = c^3 + d^3, a > b, a > c, c > d. Implies c > b.
 
 import Data.Functor
+import qualified Data.HashTable.IO as H
 import Data.Maybe
 import Data.Word
 import Math.NumberTheory.Powers.Cubes
@@ -9,21 +10,31 @@ import System.Environment
 
 type N = Word64
 
+type H = H.BasicHashTable N N
+
 -- Largest x where 2x^3 doesn't overflow.
 cubeMax :: N
 cubeMax = 2097151
 
-givenA :: N -> [(N, N, N, N)]
-givenA a = concatMap (givenAB a (a * a * a)) [1 .. a - 2]
+genH :: N -> IO H
+genH a = do
+    h <- H.newSized (fromIntegral a)
+    mapM_ (\n -> H.insert h (n * n * n) n) [1 .. a]
+    return h
 
-givenAB :: N -> N -> N -> [(N, N, N, N)]
-givenAB a a3 b = catMaybes $ map (givenABC a b a3b3) [cMin .. a - 1]
+givenA :: H -> N -> IO [(N, N, N, N)]
+givenA h a = concat <$> mapM (givenAB h a (a * a * a)) [1 .. a - 2]
+
+givenAB :: H -> N -> N -> N -> IO [(N, N, N, N)]
+givenAB h a a3 b = catMaybes <$> mapM (givenABC h a b a3b3) [cMin .. a - 1]
   where
     a3b3 = a3 + b * b * b
     cMin = ceiling $ (fromIntegral a3b3 / 2) ** (1 / 3)
 
-givenABC :: N -> N -> N -> N -> Maybe (N, N, N, N)
-givenABC a b a3b3 c = ((,,,) a b c) <$> exactCubeRoot (a3b3 - c * c * c)
+givenABC :: H -> N -> N -> N -> N -> IO (Maybe (N, N, N, N))
+givenABC h a b a3b3 c = fmap ((,,,) a b c) <$>
+    H.lookup h (a3b3 - c * c * c)
+    --exactCubeRoot (a3b3 - c * c * c)
 
 readMb :: Read a => String -> Maybe a
 readMb s = fmap fst . listToMaybe $ reads s
@@ -38,4 +49,6 @@ main = do
         Just n -> return n
         _ -> error usage
       _ -> error usage
-    mapM_ print $ givenA a
+    h <- genH a
+    res <- givenA h a
+    mapM_ print res
