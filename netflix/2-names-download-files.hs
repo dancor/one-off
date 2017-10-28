97c1c191ae76ddbdab4ef6512b231cf845b8b66a
Daniel P Corson <danl@alum.mit.edu>
2017-10-28 18:11:12 +0530

    Title; also add de and es

--- netflix/2-names-download-files.hs
+++ netflix/2-names-download-files.hs
@@ -1,44 +1,51 @@
+import Control.Applicative
+import Control.Monad
 import Data.Char
+import Data.List
 import HSH
 import Network.HTTP
 import System.Directory
+import System.Environment
 import System.Exit
 import System.FilePath
 
-import FlattenTitle
-
-openURL x = getResponseBody =<< simpleHTTP (getRequest x)
+import Title
 
 myRun :: (String, [String]) -> IO ExitCode
 myRun = run
 
+userAgent :: String
 userAgent = "--user-agent=User-Agent: Mozilla/5.0 " ++
     "(Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " ++
     "(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
 
-doWget flatTitle = do
-    putStrLn $ "Getting: " ++ flatTitle
+isCached :: Title -> IO Bool
+isCached (Title t) = doesFileExist $ "cached" </> t
+
+getTitleEvenIfCached :: Int -> Int -> Title -> IO ()
+getTitleEvenIfCached tot cur (Title t) = do
+    putStrLn $ "Getting (" ++ show cur ++ "/" ++ show tot ++ "): " ++ t
     cd "m"
     myRun ("wget",
-        [ "-nc", "--wait=5", "--random-wait", userAgent
-        , "http://www.rottentomatoes.com/m/" ++ flatTitle
+        [ "-nc", "--wait=3", "--random-wait", userAgent
+        , "http://www.rottentomatoes.com/m/" ++ t
         ])
     cd ".."
     cd "tv"
     myRun ("wget",
-        [ "-nc", "--wait=5", "--random-wait", userAgent
-        , "http://www.rottentomatoes.com/tv/" ++ flatTitle
+        [ "-nc", "--wait=3", "--random-wait", userAgent
+        , "http://www.rottentomatoes.com/tv/" ++ t
         ])
     cd ".."
 
-getTitle title = do
-    let flatTitle = flattenTitle title
-    isCached <- doesFileExist $ "cached" </> flatTitle
-    if isCached
-      then putStrLn $ "Cached: " ++ flatTitle
-      else doWget flatTitle
-
 main :: IO ()
 main = do
-    titles <- fmap lines $ getContents
-    mapM_ getTitle titles
+    args <- getArgs
+    titles1 <- map flattenTitle . concatMap lines <$> mapM readFile args
+    print $ show (length titles1) ++ " titles before dedupe."
+    let titles2 = nub titles1
+    print $ show (length titles2) ++ " titles after dedupe."
+    titles3 <- filterM (fmap not . isCached) titles2
+    let t3l = length titles3
+    print $ show t3l ++ " titles after cache check."
+    zipWithM_ (getTitleEvenIfCached t3l) [1..] titles3
--- netflix/4-names-to-mark-cached.hs
+++ netflix/4-names-to-mark-cached.hs
@@ -1,9 +1,12 @@
+import Control.Applicative
 import Data.Char
+import System.Environment
 import System.FilePath
 
 import FlattenTitle
 
 main :: IO ()
 main = do
-    titles <- fmap (map flattenTitle . lines) $ getContents
-    mapM_ (\title -> writeFile ("cached" </> title) "") titles
+    args <- getArgs
+    titles <- map flattenTitle . concatMap lines <$> mapM readFile args
+    mapM_ (\(Title t) -> writeFile ("cached" </> t) "") titles
