ecfb6f7cf2b1f56fee983ab4b7780ad81bbb4146
Daniel P Corson <danl@alum.mit.edu>
2018-10-30 17:36:50 -0700

    cp

---
+++ hanyu-hear-me/make-audio-qs.hs
@@ -0,0 +1,53 @@
+#include <h>
+
+data Ci = Ci {
+    ciNum :: Int,
+    ciHanzi :: DT.Text,
+    ciDef :: DT.Text} deriving (Show, Generic)
+
+instance Ae.ToJSON Ci
+instance Ae.FromJSON Ci
+
+type HomonymSet = (DT.Text, [Ci])
+
+type HomonymSets = [HomonymSet]
+
+jencCi :: Ci -> DT.Text
+jencCi (Ci n h d) = "{n:" <> DT.pack (show n) <> ",h:\"" <> h <> "\",d:\"" <>
+    DT.replace "\"" "\\\"" (DT.replace "\\" "\\\\" d) <> "\"}"
+
+jencHomonymSet :: HomonymSet -> DT.Text
+jencHomonymSet (pinyin, cis) = "\"" <> pinyin <> "\":[" <> 
+    DT.intercalate "," (map jencCi cis) <> "]"
+
+jencHomonymSets :: HomonymSets -> DT.Text
+jencHomonymSets hs = "{" <> DT.intercalate "," (map jencHomonymSet hs) <> "}"
+
+cleanDef =
+    DT.replace "<\21160>" "V." .
+    DT.replace "<\21517>" "N." .
+    DT.replace "<\37327>" "M." .
+    DT.replace "<\24418>" "S.V." .
+    DT.replace "<\21475>" "COLL." .
+    DT.replace "<\21103>" "ADV." .
+    DT.replace "<\36830>" "CONJ."
+
+procLine :: Int -> DT.Text -> (DT.Text, [Ci])
+procLine n l = (pinyin, [Ci n hanzi (cleanDef def)])
+  where hanzi:pinyin:def:_ = DT.splitOn "\t" l
+
+--procEl :: [(Int, DT.Text, DT.Text)] -> Maybe (Int, [(DT.Text, DT.Text)])
+--procEl l = if any over2300 l then Just $ map bcOfAbc $ reverse l else Nothing
+procEl l = Just (minimum $ map aOfAbc l, reverse l)
+  where
+    over2300 (n, _, _) = n > 2300
+    aOfAbc (a, _, _) = a
+    bcOfAbc (_, b, c) = (b, c)
+
+main = do
+    m <- sortBy (compare `on` ciNum . head . snd) .
+        HMS.toList . HMS.map reverse . HMS.fromListWith (++) .
+        zipWith procLine [1..] . DT.lines <$> DTI.readFile "all.txt"
+    BSL.writeFile "out" . ("var homonymSets = " <>) . (<> ";\n") .
+        Ae.encode $
+        filter (any (> 2300) . map ciNum . snd) m
