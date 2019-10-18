23c88d63786588da333e74d1a4b2186312838a8c
Daniel P Corson <danl@alum.mit.edu>
2019-10-17 21:54:15 -0700

    cp

--- try-accelerate/app/Main.hs
+++ try-accelerate/app/Main.hs
@@ -2,15 +2,21 @@
 
 module Main where
 
-import Lib
 import Data.Array.Accelerate              as A
 import Data.Array.Accelerate.LLVM.Native  as CPU
 import Data.Array.Accelerate.LLVM.PTX     as GPU
+import Data.List
 import Formatting
 import Formatting.Clock
+import Prelude                            as P
 import System.Clock
 
-dotp :: Acc (Vector Float) -> Acc (Vector Float) -> Acc (Scalar Float)
+wWidth, wHeight :: Int
+wWidth =  2560
+wHeight = 1440
+type World = Matrix Int
+
+dotp :: Acc (Vector T) -> Acc (Vector T) -> Acc (Scalar T)
 dotp xs ys = A.fold (+) 0 (A.zipWith (*) xs ys)
 
 mill, ten_mill, hundo_mill :: Int
@@ -18,18 +24,24 @@ mill       = 1000000
 ten_mill   = 10000000
 hundo_mill = 100000000
 
+type T = Int8
+
 main :: IO ()
 main = do
     --             CPU   GPU   winner (4-core CPU vs. GTX1060-6GB)
     -- mill        46ms  81ms CPUx2   (CPU wins even more for smaller samples)
     -- ten_mill   340ms 100ms GPUx3.4
     -- hundo_mill 4s    250ms GPUx16
-    let xs = fromList (Z:.ten_mill) [0..]   :: Vector Float
-    let ys = fromList (Z:.ten_mill) [1,3..] :: Vector Float
+    let xs = fromList (Z:.ten_mill) (repeat 67)   :: Vector T
+    let ys = fromList (Z:.ten_mill) (repeat 55) :: Vector T
     t1 <- getTime Monotonic
-    print $ CPU.run $ dotp (use xs) (use ys)
+    print $
+        foldl' (+) 0 (P.zipWith (*) (P.take ten_mill (repeat 67)) (repeat 55))
     t2 <- getTime Monotonic
-    print $ GPU.run $ dotp (use xs) (use ys)
+    print $ CPU.run $ dotp (use xs) (use ys)
     t3 <- getTime Monotonic
-    fprint ("CPU: " % timeSpecs % "\n") t1 t2
-    fprint ("GPU: " % timeSpecs % "\n") t2 t3
+    print $ GPU.run $ dotp (use xs) (use ys)
+    t4 <- getTime Monotonic
+    fprint ("CPU1: " % timeSpecs % "\n") t1 t2
+    fprint ("CPUm: " % timeSpecs % "\n") t2 t3
+    fprint ("GPU:  " % timeSpecs % "\n") t3 t4
