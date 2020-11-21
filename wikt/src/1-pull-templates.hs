6493b5e89a62d0074b8b85463b652b9130d9e209
Daniel P Corson <danl@alum.mit.edu>
2020-11-20 19:33:51 -0500

    cp

--- wikt/src/1-pull-templates.hs
+++ wikt/src/1-pull-templates.hs
@@ -3,21 +3,24 @@ import Share
 
 readStart :: Ts -> [Ts]
 readStart [] = []
-readStart (x:xs) = if isTitle x then readFromTitle x xs else readStart xs
+readStart (x:xs) = case procTemplate x of
+  Just t -> readFromTitle t xs
+  _ -> readStart xs
 
 readFromTitle :: T -> Ts -> [Ts]
 readFromTitle t [] = error "End of input after title before text"
-readFromTitle t (x:xs) = if textPre `TL.isPrefixOf` x
-  then readFromText [] (procText t) xs
-  else readFromTitle t xs
+readFromTitle t (x:xs) = case procText x of
+  Just t2 -> readFromText [t] t2 xs
+  _       -> readFromTitle t xs
 
 readFromText :: Ts -> T -> Ts -> [Ts]
 readFromText acc acc1 [] = error "End of input in section"
 readFromText acc acc1 (x:xs) = if shaPre `TL.isPrefixOf` x
-  then (acc ++ [TL.drop (TL.length textPost) acc1]) : readStart xs
+  then (acc ++ [TL.dropEnd (TL.length textPost) acc1]) : readStart xs
   else readFromText (acc ++ [acc1]) x xs
 
 main :: IO ()
 main = do
     pages <- readStart . TL.lines <$> TL.getContents
-    mapM_ (\(t:xs) -> putStrLn $ show t <> show (TL.unlines xs)) pages
+    mapM_ (\(t:xs) -> putStrLn $ show t <> show (TL.unlines xs))
+        pages
--- wikt/src/2-pull-lang.hs
+++ wikt/src/2-pull-lang.hs
@@ -1,31 +1,18 @@
 #include <h>
 import Share
-type Ts = [T]
-type T = TL.Text
-
-titlePre, titlePost, shaPre, textPost :: T
-titlePre = "    <title>"
-titlePost = "</title>"
-shaPre = "      <sha1>"
-textPost = "</text>"
-
-isTitle, isH2 :: T -> Bool
-isTitle = (titlePre `TL.isPrefixOf`)
-isH2 x = TL.take 2 x == "==" && TL.drop 2 (TL.take 3 x) /= "="
-
-procTitle :: T -> T
-procTitle = TL.dropEnd (TL.length titlePost) . TL.drop (TL.length titlePre)
 
 readStart :: T -> Ts -> [Ts]
 readStart _ []     = []
-readStart l (x:xs) = if isTitle x then readFromTitle l x xs else readStart l xs
+readStart l (x:xs) = case procTitle x of
+  Just t -> readFromTitle l t xs
+  _ -> readStart l xs
 
 readFromTitle :: T -> T -> Ts -> [Ts]
 readFromTitle _ t []     = []
-readFromTitle l t (x:xs) = if isTitle x
-  then readFromTitle l x xs
-  else if x == "==" <> l <> "=="
-    then readFromSection l [] (procTitle t) xs
+readFromTitle l t (x:xs) = case procTitle x of
+  Just t -> readFromTitle l t xs
+  _ -> if x == "==" <> l <> "=="
+    then readFromSection l [] t xs
     else readFromTitle l t xs
 
 readFromSection :: T -> Ts -> T -> Ts -> [Ts]
@@ -33,7 +20,7 @@ readFromSection _ _   _    []     = error "End of input in section"
 readFromSection l acc acc1 (x:xs) = if isH2 x
   then (acc ++ [acc1]) : readStart l xs
   else if shaPre `TL.isPrefixOf` x
-    then (acc ++ [TL.drop (TL.length textPost) acc1]) : readStart l xs
+    then (acc ++ [TL.dropEnd (TL.length textPost) acc1]) : readStart l xs
     else readFromSection l (acc ++ [acc1]) x xs
 
 main :: IO ()
