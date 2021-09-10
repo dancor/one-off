2345989a0f1476e28dc3eab023a7b89b5bd306af
Daniel P Corson <danl@alum.mit.edu>
2021-09-10 10:59:09 +0100

    cp

---
+++ try-cpp/a.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << sizeof(int) << endl;
+    cout << sizeof(long) << endl;
+    cout << sizeof(int*) << endl;
+    cout << sizeof(size_t) << endl;
+}
