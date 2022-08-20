8130767a0a4ab2fa19bdbb7517b285a8a3d3d8ec
Daniel P Corson <danl@alum.mit.edu>
2022-08-20 19:08:57 +1000

    cp

--- piano/a.py
+++ piano/a.py
@@ -6,91 +6,90 @@ from pydub import AudioSegment
 import pyxhook
 from sofa.read_sf2 import fluidsynth
 import sofa.read_sf2.read_sf2 as s
-l = s.sf2_loader('/home/danl/Documents/MuseScore2/Soundfonts/SGM-v2.01-YamahaGrand-Guit-Bass-v2.7.sf2')
-
+l = s.sf2_loader('/home/danl/Documents/MuseScore2/Soundfonts/' +
+    'SGM-v2.01-YamahaGrand-Guit-Bass-v2.7.sf2')
 kToNote = {}
-
 # Paul singing range: G2-G4
-
-def onKeyUp(e):
-    print("up: ", e.Key)
+def e2n(e):
+    k = e.Key
+    if k == 'z': return 36 # "C2"
+    if k == 'x': return 37 # "C#2"
+    if k == 'c': return 38 # "D2"
+    if k == 'v': return 39 # "D#2"
+    if k == 'b': return 40 # "E2"
+    if k == 'k': return 41 # "F2"
+    if k == 'm': return 42 # "F#2"
+    if k == 'comma': return 43 # "G2"
+    if k == 'period': return 44 # "G#2"
+    if k == 'slash': return 45 # "A2"
+    if k == 'Shift_R': return 46 # "A#2"
+    if k == 'Up': return 47 # "B2"
+    if k == 'a': return 48 # "C3
+    if k == 'r': return 49 # "C#3"
+    if k == 's': return 50 # "D3"
+    if k == 't': return 51 # "D#3"
+    if k == 'd': return 52 # "E3"
+    if k == 'h': return 53 # "F3"
+    if k == 'n': return 54 # "F#3"
+    if k == 'e': return 55 # "G3"
+    if k == 'i': return 56 # "G#3"
+    if k == 'o': return 57 # "A3"
+    #if k == 'apostrophe': return 58 # "A#3"
+    if k == 'Multi_key': return 58 # "A#3"
+    if k == 'Return': return 59 # "B3"
+    if k == 'q': return 60 # "C4"
+    if k == 'w': return 61 # "C#4"
+    if k == 'f': return 62 # "D4"
+    if k == 'p': return 63 # "D#4"
+    if k == 'g': return 64 # "E4"
+    if k == 'j': return 65 # "F4"
+    if k == 'l': return 66 # "F#4"
+    if k == 'u': return 67 # "G4"
+    if k == 'y': return 68 # "G#4"
+    if k == 'semicolon': return 69 # "A4"
+    if k == 'bracketleft': return 70 # "A#4"
+    if k == 'bracketright': return 71 # "B4"
+    if k == '1': return 72 # "C5"
+    if k == '2': return 73 # "C#5"
+    if k == '3': return 74 # "D5"
+    if k == '4': return 75 # "D#5"
+    if k == '5': return 76 # "E5"
+    if k == '6': return 77 # "F5"
+    if k == '7': return 78 # "F#5"
+    if k == '8': return 79 # "G5"
+    if k == '9': return 80 # "G#5"
+    if k == '0': return 81 # "A5"
+    if k == 'minus': return 82 # "A#5"
+    if k == 'equal': return 83 # "B5"
+    if k == 'F1': return 84 # "C6"
+    if k == 'F2': return 85 # "C#6"
+    if k == 'F3': return 86 # "D6"
+    if k == 'F4': return 87 # "D#6"
+    if k == 'F5': return 88 # "E6"
+    if k == 'F6': return 89 # "F6"
+    if k == 'F7': return 90 # "F#6"
+    if k == 'F8': return 91 # "G6"
+    if k == 'F9': return 92 # "G#6"
+    if k == 'F10': return 93 # "A6"
+    if k == 'F11': return 94 # "A#6"
+    if k == 'F12': return 95 # "B6"
+    if k == 'Pause': return 96 # "C7"
+    if k == 'Insert': return 97 # "C#7"
+    if k == 'Delete': return 98 # "D7"
+    if k == 'Home': return 99 # "D#7"
+    if k == 'Page_Up': return 100 # "E7"
+    if k == 'Next': return 101 # "F7"
+    if k == 'End': return 102 # "F#7"
+    if k == 'Right': return 103 # "G7"
+    if k == 'Down': return 104 # "G#7"
+    if k == 'Left': return 105 # "A7"
+    if k == 'Super_R': return 106 # "A#7"
+    if k == 'Control_R': return 107 # "B7"
+    if k == 'Alt_R': return 108 # "C8"
+    return 0
+def onKeyUp(e): print("up: ", e.Key)
 def onKeyDown(e):
-    print("down", e.Key)
-    n = 0
-    if   e.Key == 'z': n = 36 # "C2"
-    elif e.Key == 'x': n = 37 # "C#2"
-    elif e.Key == 'c': n = 38 # "D2"
-    elif e.Key == 'v': n = 39 # "D#2"
-    elif e.Key == 'b': n = 40 # "E2"
-    elif e.Key == 'k': n = 41 # "F2"
-    elif e.Key == 'm': n = 42 # "F#2"
-    elif e.Key == 'comma': n = 43 # "G2"
-    elif e.Key == 'period': n = 44 # "G#2"
-    elif e.Key == 'slash': n = 45 # "A2"
-    elif e.Key == 'Shift_R': n = 46 # "A#2"
-    elif e.Key == 'Up': n = 47 # "B2"
-    elif e.Key == 'a': n = 48 # "C3
-    elif e.Key == 'r': n = 49 # "C#3"
-    elif e.Key == 's': n = 50 # "D3"
-    elif e.Key == 't': n = 51 # "D#3"
-    elif e.Key == 'd': n = 52 # "E3"
-    elif e.Key == 'h': n = 53 # "F3"
-    elif e.Key == 'n': n = 54 # "F#3"
-    elif e.Key == 'e': n = 55 # "G3"
-    elif e.Key == 'i': n = 56 # "G#3"
-    elif e.Key == 'o': n = 57 # "A3"
-    #elif e.Key == 'apostrophe': n = 58 # "A#3"
-    elif e.Key == 'Multi_key': n = 58 # "A#3"
-    elif e.Key == 'Return': n = 59 # "B3"
-    elif e.Key == 'q': n = 60 # "C4"
-    elif e.Key == 'w': n = 61 # "C#4"
-    elif e.Key == 'f': n = 62 # "D4"
-    elif e.Key == 'p': n = 63 # "D#4"
-    elif e.Key == 'g': n = 64 # "E4"
-    elif e.Key == 'j': n = 65 # "F4"
-    elif e.Key == 'l': n = 66 # "F#4"
-    elif e.Key == 'u': n = 67 # "G4"
-    elif e.Key == 'y': n = 68 # "G#4"
-    elif e.Key == 'semicolon': n = 69 # "A4"
-    elif e.Key == 'bracketleft': n = 70 # "A#4"
-    elif e.Key == 'bracketright': n = 71 # "B4"
-    elif e.Key == '1': n = 72 # "C5"
-    elif e.Key == '2': n = 73 # "C#5"
-    elif e.Key == '3': n = 74 # "D5"
-    elif e.Key == '4': n = 75 # "D#5"
-    elif e.Key == '5': n = 76 # "E5"
-    elif e.Key == '6': n = 77 # "F5"
-    elif e.Key == '7': n = 78 # "F#5"
-    elif e.Key == '8': n = 79 # "G5"
-    elif e.Key == '9': n = 80 # "G#5"
-    elif e.Key == '0': n = 81 # "A5"
-    elif e.Key == 'minus': n = 82 # "A#5"
-    elif e.Key == 'equal': n = 83 # "B5"
-    elif e.Key == 'F1': n = 84 # "C6"
-    elif e.Key == 'F2': n = 85 # "C#6"
-    elif e.Key == 'F3': n = 86 # "D6"
-    elif e.Key == 'F4': n = 87 # "D#6"
-    elif e.Key == 'F5': n = 88 # "E6"
-    elif e.Key == 'F6': n = 89 # "F6"
-    elif e.Key == 'F7': n = 90 # "F#6"
-    elif e.Key == 'F8': n = 91 # "G6"
-    elif e.Key == 'F9': n = 92 # "G#6"
-    elif e.Key == 'F10': n = 93 # "A6"
-    elif e.Key == 'F11': n = 94 # "A#6"
-    elif e.Key == 'F12': n = 95 # "B6"
-    elif e.Key == 'Pause': n = 96 # "C7"
-    elif e.Key == 'Insert': n = 97 # "C#7"
-    elif e.Key == 'Delete': n = 98 # "D7"
-    elif e.Key == 'Home': n = 99 # "D#7"
-    elif e.Key == 'Page_Up': n = 100 # "E7"
-    elif e.Key == 'Next': n = 101 # "F7"
-    elif e.Key == 'End': n = 102 # "F#7"
-    elif e.Key == 'Right': n = 103 # "G7"
-    elif e.Key == 'Down': n = 104 # "G#7"
-    elif e.Key == 'Left': n = 105 # "A7"
-    elif e.Key == 'Super_R': n = 106 # "A#7"
-    elif e.Key == 'Control_R': n = 107 # "B7"
-    elif e.Key == 'Alt_R': n = 108 # "C8"
+    n = e2n(e)
     if n:
         l.synth.noteon(0, n, 100)
         arrs = []
@@ -102,9 +101,8 @@ def onKeyDown(e):
         a = AudioSegment.from_raw(BytesIO(samps),
             sample_width=2, channels=2, frame_rate=44100)
         mp.pygame.mixer.Sound(buffer=a.raw_data).play()
-  
-new_hook = pyxhook.HookManager()
-new_hook.KeyDown = onKeyDown
-new_hook.KeyUp = onKeyUp
-new_hook.HookKeyboard()
-new_hook.start()
+hook = pyxhook.HookManager()
+hook.KeyDown = onKeyDown
+hook.KeyUp = onKeyUp
+hook.HookKeyboard()
+hook.start()
