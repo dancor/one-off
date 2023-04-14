4374fa40c839e9111ae69ea35a49111a6a96ebd8
Daniel P Corson <danl@alum.mit.edu>
2023-04-14 12:40:18 +0100

    cp

--- zim/a.c
+++ zim/a.c
@@ -6,21 +6,26 @@
 #include <zim/entry.h>
 #include <zim/item.h>
 using namespace std;
-const char*preGood1 = "    <summary class=\"section-heading\"><h2 id=\"Polish";
-const char*preGood2 = "    <summary class=\"section-heading\"><h2 id=\"English";
+const char*pre1 = "    <summary class=\"section-heading\"><h2 id=\"English";
+const char*pre2 = "    <summary class=\"section-heading\"><h2 id=\"German";
+const char*pre3 = "    <summary class=\"section-heading\"><h2 id=\"Polish";
+const char*pre4 = "    <summary class=\"section-heading\"><h2 id=\"Spanish";
 const char*pre = "    <summary class=\"section-heading\"><h2 id=\"";
 int main(int argc, char**argv) {try {
   zim::Archive a("/home/d/data/wik/t/en.zim");
   ofstream f; f.open("/home/d/tmp/zim.htm");
-  auto data = a.getEntryByTitle(argv[1]).getItem().getData();
+  auto arg = argv[1]; while (arg[0] == '/') arg++;
+  auto data = a.getEntryByTitle(arg).getItem().getData();
   int cN = data.size() + 1; char*c = (char*)malloc(cN);
   memcpy(c, data.data(), cN - 1); c[cN - 1] = 0;
   stringstream ss(c);
   string l;
   bool copy = true;
   while (getline(ss, l, '\n')) {
-    if (!l.rfind(preGood1, 0)) copy = true;
-    else if (!l.rfind(preGood2, 0)) copy = true;
+    if (!l.rfind(pre1, 0)) copy = true;
+    else if (!l.rfind(pre2, 0)) copy = true;
+    else if (!l.rfind(pre3, 0)) copy = true;
+    else if (!l.rfind(pre4, 0)) copy = true;
     else if (!l.rfind(pre, 0)) copy = false;
     if (copy) f << l << "\n";
   }
@@ -30,7 +35,6 @@ int main(int argc, char**argv) {try {
   //f << int(c[cN - 1]) << "\n";
   */
   f.close();
-  } catch (const std::exception& e) {cerr << e.what() << ". LOL\n";}
   const char*cmd = "/home/danl/bin/ff";
   execl(cmd, cmd, "/home/d/tmp/zim.htm", NULL);
-}
+} catch (const std::exception& e) {cerr<<argv[1]<<": "<<e.what()<<"\n";}}
