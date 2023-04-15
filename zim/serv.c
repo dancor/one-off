683ca4e2f8648f537704a69ba130ebdb34edf6ff
Daniel P Corson <danl@alum.mit.edu>
2023-04-15 15:54:01 +0100

    cp

--- zim/serv.c
+++ zim/serv.c
@@ -11,7 +11,8 @@
 using namespace std;
 const int one = 1;
 const char *pre  = "    <summary class=\"section-heading\"><h2 id=\"",
-  *http = "HTTP/1.1 200 OK\r\n Content-type:text/html\r\n Content-length: ";
+  *http = "HTTP/1.1 200 OK\r\n Content-type:text/html\r\n Content-length: ",
+  *s1 = "<html><body>", *s2 = ": no entry</body></html>";
 inline char h2i(char h) {return h - (h < 65 ? 48 : 55);}
 inline void percentDecode(char *s) {char *t = s;
 doNextChar:
@@ -24,7 +25,7 @@ int main(int argc, char **argv) {
   string r;
   char *c, *c1, *c2, *c3, *d, *d1, cliIp[32], m[1024]; ptrdiff_t diff;
   zim::Archive arc("/home/d/data/wik/t/en.zim"); zim::Blob data;
-  int cN, conn, dLen, s = socket(PF_INET, SOCK_STREAM, 0); u8 copy;
+  int cN, conn, cLen, dLen, s = socket(PF_INET, SOCK_STREAM, 0), wdLen; u8 copy;
   nie(setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int)));
   struct sockaddr_in ca, sa; bzero(&sa, sizeof(sa)); sa.sin_family = AF_INET;
   sa.sin_addr.s_addr = htonl(INADDR_ANY); sa.sin_port = htons(atoi(argv[1]));
@@ -47,7 +48,6 @@ awaitClient:
         cN = data.size(); c = (char*)malloc(cN + 999);
         d = (char*)malloc(cN + 1); d1 = d;
         memcpy(c, data.data(), cN); c[cN] = 0; c1 = c; copy = 1;
-        //char*e=escStr(c);printf("c[%.*s]\n",99,e);free(e);
 doLine:
         //c2 = c + 1600; c2[200] = 0; printf("%s\n", c2);
         if (startsW(c1, pre)) {
@@ -63,6 +63,7 @@ doLine:
           goto doLine;
         }
         strcpy(d1, c1); dLen = (int)strlen(d);
+        //printf("dLen[%d]\n", dLen);
         snprintf(c, 2 * cN, "%s%d\r\n\r\n%.*s", http, dLen, dLen, d);
         sendto(conn, c, strlen((char*)c), 0, (struct sockaddr*)&sa, al);
         free(c); free(d);
@@ -73,5 +74,13 @@ doLine:
         r += data.data();
         sendto(conn, r.c_str(), r.length(), 0, (struct sockaddr*)&sa, al);
       }
+    } else {
+      wdLen = strlen(wd); dLen = strlen(s1) + wdLen + strlen(s2);
+      d = (char*)malloc(dLen + 1); snprintf(d, dLen + 1, "%s%s%s", s1, wd, s2);
+      cLen = strlen(http) + 999 + dLen; cN = cLen + 1; c = (char*)malloc(cN);
+      snprintf(c, cN, "%s%d\r\n\r\n%s", http, dLen, d);
+      //char*e=escStr(c);printf("c[%.*s]\n",199,e);free(e);
+      sendto(conn, c, cLen, 0, (struct sockaddr*)&sa, al);
+      free(c); free(d);
     }
   } close(conn); goto awaitClient;}
