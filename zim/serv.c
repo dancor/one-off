60540c466b6c4808132576c452cc783059c1e29d
Daniel P Corson <danl@alum.mit.edu>
2023-04-16 14:40:06 +0100

    cp

--- zim/serv.c
+++ zim/serv.c
@@ -12,19 +12,20 @@ using namespace std;
 const int one = 1;
 const char *pre  = "    <summary class=\"section-heading\"><h2 id=\"",
   *http = "HTTP/1.1 200 OK\r\n Content-type:text/html\r\n Content-length: ",
+  *postWd = " HTTP/1.1", *enwik = "enwik",
   *s1 = "<html><body>", *s2 = ": no entry</body></html>";
 inline char h2i(char h) {return h - (h < 65 ? 48 : 55);}
 inline void percentDecode(char *s) {char *t = s;
-doNextChar:
+percentDecode:
   if (!s[0]) {t[0] = 0; return;}
   if (s[0] == '%') {t[0] = 16 * h2i(s[1]) + h2i(s[2]);
-    s += 3; t++; goto doNextChar;}
-  t[0] = s[0]; s++; t++; goto doNextChar;
-}
+    s += 3; t++; goto percentDecode;}
+  t[0] = s[0]; s++; t++; goto percentDecode;}
 int main(int argc, char **argv) {
   string r;
   char *c, *c1, *c2, *c3, *d, *d1, cliIp[32], m[1024]; ptrdiff_t diff;
-  zim::Archive arc("/home/d/data/wik/t/en.zim"); zim::Blob data;
+  zim::Archive enwikt("/home/d/data/wik/t/en.zim"),
+    enwiki("/home/d/data/wik/i/en.zim"); zim::Blob data;
   int cN, conn, cLen, dLen, s = socket(PF_INET, SOCK_STREAM, 0), wdLen; u8 copy;
   nie(setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int)));
   struct sockaddr_in ca, sa; bzero(&sa, sizeof(sa)); sa.sin_family = AF_INET;
@@ -37,11 +38,13 @@ awaitClient:
   memset(m, 0, sizeof(m));
   if (recvfrom(conn, m, sizeof(m), 0, (struct sockaddr*)&ca, &al) > 0) {
     char *wd = m + 5; percentDecode(wd); while (wd[0] == '/') wd++;
-    u8 haveData, full = 0;
+    //char*e=escStr(wd);printf("wd[%.*s]\n",199,wd);free(e);
+    u8 haveData = 0, full = 0;
     if (wd[0] == 'f' && wd[1] == '/') {wd += 2; full = 1;}
-    char *wdEnd = wd; while (wdEnd[0] != ' ') wdEnd++; wdEnd[0] = '\0';
-    try {data = arc.getEntryByTitle((char*)wd).getItem().getData();
-      haveData = 1;} catch (const std::exception& e) {haveData = 0;}
+    char *wdEnd = strchr(wd, '\n');
+    if (wdEnd) {wdEnd[-strlen(postWd) - 1] = 0;
+      try {data = enwiki.getEntryByTitle((char*)wd).getItem().getData();
+        haveData = 1;} catch (const std::exception& e) {}}
     if (haveData) {
       if (!full) {
         cN = data.size(); c = (char*)malloc(cN + 999);
@@ -80,7 +83,6 @@ doLine:
       snprintf(c, cN, "%s%d\r\n\r\n%s", http, dLen, d); cLen = strlen(c);
       sendto(conn, c, cLen, 0, (struct sockaddr*)&sa, al);
       //printf("dLen[%d] cLen[%d]\n", dLen, cLen);
-      //char*e=escStr(c);printf("c[%.*s]\n",199,e);free(e);
       free(c); free(d);
     }
   } close(conn); goto awaitClient;}
