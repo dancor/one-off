d67ec44401b90dee514ac34485767fe122c78350
Daniel P Corson <danl@alum.mit.edu>
2023-09-10 09:46:07 -1000

    cp

--- fluxbox-keys/a.hs
+++ fluxbox-keys/a.hs
@@ -1,5 +1,4 @@
 #include <h>
-
 -- Mod4 codes:
 -- b: black-make-terminal
 -- m: make-terminal
@@ -7,36 +6,31 @@
 -- t: travel-window-to
 -- s: switch-focus
 -- z: zap-invisible
-
 type T = Text
-
 main :: IO ()
 --main = T.writeFile "/home/danl/.fluxbox/keys" $ T.unlines $ genCodes :
 main = T.writeFile "out" $ T.unlines $ genCodes :
     map ("Mod4 " <>) mod4Codes ++ map ("Control Mod1 " <>) mod4Codes
-
 sh :: Show a => a -> T
 sh = T.pack . show
-
 i2f :: Int -> Float
 i2f = fromIntegral
-
 ms, ss, ts :: [T]
 ms = concatMap (\(a,s) -> [
-    "m " <> a <> " :Exec multiplexed-positioned-titled-xt " <> s,
-    "b " <> a <> " :Exec multiplexed-positioned-titled-xt -b " <> s]) [
-    ("a", "1 0 a1"),
-    ("r", "2 1 a2"),
-    ("s", "3 2 a3"),
-    ("t", "4 3 a4"),
-    ("d", "5 4 a5"),
-    ("h", "6 's2 0' b1"),
-    ("n", "7 's2 1' b2"),
-    ("e", "8 's2 2' b3"),
-    ("i", "9 's2 3' b4"),
-    ("o", "10 's2 4' b5")] 
+  "m " <> a <> " :Exec multiplexed-positioned-titled-xt " <> s,
+  "b " <> a <> " :Exec multiplexed-positioned-titled-xt -b " <> s]) [
+  ("a", "1 0 a1"),
+  ("r", "2 1 a2"),
+  ("s", "3 2 a3"),
+  ("t", "4 3 a4"),
+  ("d", "5 4 a5"),
+  ("h", "6 's2 0' b1"),
+  ("n", "7 's2 1' b2"),
+  ("e", "8 's2 2' b3"),
+  ("i", "9 's2 3' b4"),
+  ("o", "10 's2 4' b5")] 
 ss = concatMap (ssH . T.singleton) ("abcdefghinoprstw" :: String) where
-    ssH t = ["s " <> t <> " :Exec wf " <> t, "z " <> t <> " :Exec wf -z " <> t]
+  ssH t = ["s " <> t <> " :Exec wf " <> t, "z " <> t <> " :Exec wf -z " <> t]
 ts = map("t "<>)$
   [      x2k x<>(if x<15 then " "<>x2k w else "")<>wcmd x 0 w 16|
     x<-[0..15],w<-[1..16-x]]++
@@ -47,25 +41,22 @@ ts = map("t "<>)$
   ["p "<>x2k x<>(if x<15 then " "<>x2k w else "")<>wcmd x 8 w 8|
     x<-[0..15],w<-[1..16-x]]
   where
-  --x2k x = "`1234567890-="!!x
-  --x2k x=("grave":map T.singleton"1234567890"++["minus","equal"])!!(x`mod`13)
-  --y2k y=("grave":map T.singleton"1234567890"++["minus","equal"])!!(y`mod`13)
   x2k x = T.singleton$"0123456789abcdef"!!(x`mod`16)
   y2k=x2k
   wcmd x y w h = " :Exec wmctrl -r :ACTIVE: -e 0," <>
     T.intercalate "," (map sh [160*x,90*y,160*w,90*h])
-  {-
-  wcmd x y w h = " :Exec wmctrl -r :ACTIVE: -e 0," <>
-    T.intercalate "," (map sh [xPix,120*y,min wPixMax (200*w),120*h])
-    where xPix = 200 * x; wPixMax = 2560 - xPix
-  tsH :: Int -> Int -> [T]
-  tsH lpos width = forms $ sh lpos <> " " <> sh width <>
-      " :Exec wmctrl -r :ACTIVE: -e 1," <>
-      sh (i2f lpos / 2) <>
-      if lpos + width == 9 then "+" else "+" <> sh (i2f width / 2)
-  forms :: T -> [T]
-  forms x = ["t " <> x, "t q " <> x <> " +.5", "t w " <> x <> " +.87", 
-      "t z " <> x <> " .5+"]-}
+{-
+v 0 2 3 0 1 :Exec wmv 0      +0.333
+v 0 2 3 1 1 :Exec wmv 0 0.333+0.334 # hack fit
+v 0 2 3 2 1 :Exec wmv 0 0.666+0.335 # hack fit
+v 0 2 3 0 2 :Exec wmv 0      +0.668 # hack fit
+v 0 2 3 1 2 :Exec wmv 0 0.333+0.668 # hack fit
+v 9 3 0 1 :Exec wmv 0+      +0.333
+v 9 3 1 1 :Exec wmv 0+ 0.333+0.334 # hack fit
+v 9 3 2 1 :Exec wmv 0+ 0.666+0.335 # hack fit
+v 9 3 0 2 :Exec wmv 0+      +0.668 # hack fit
+v 9 3 1 2 :Exec wmv 0+ 0.333+0.668 # hack fit
+-}
 
 mod4Codes :: [T]
 mod4Codes = T.lines [r|d :ToggleDecor
@@ -96,17 +87,7 @@ r h :Exec ff -f -p 2
 r i :Exec ff -g -p 2
 r j :Exec ff -f -p s
 r k :Exec ff -g -p s
-r x :Exec xt
-v 0 2 3 0 1 :Exec wmv 0      +0.333
-v 0 2 3 1 1 :Exec wmv 0 0.333+0.334 # hack fit
-v 0 2 3 2 1 :Exec wmv 0 0.666+0.335 # hack fit
-v 0 2 3 0 2 :Exec wmv 0      +0.668 # hack fit
-v 0 2 3 1 2 :Exec wmv 0 0.333+0.668 # hack fit
-v 9 3 0 1 :Exec wmv 0+      +0.333
-v 9 3 1 1 :Exec wmv 0+ 0.333+0.334 # hack fit
-v 9 3 2 1 :Exec wmv 0+ 0.666+0.335 # hack fit
-v 9 3 0 2 :Exec wmv 0+      +0.668 # hack fit
-v 9 3 1 2 :Exec wmv 0+ 0.333+0.668 # hack fit|] ++ ms ++ ss ++ ts
+r x :Exec xt|] ++ ms ++ ss ++ ts
 
 genCodes :: T
 genCodes = [r|Mod1 Tab :NextWindow groups
