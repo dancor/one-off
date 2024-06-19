ad5144273acf8949435d78d3de0a654ffb4fcde2
Daniel P Corson <danl@alum.mit.edu>
2024-06-19 11:19:01 +1000

    cp

---
+++ trystuff/b.c
@@ -0,0 +1,11 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include "err.h"
+#include "types.h"
+int main(void){
+  printf("hi.\n");
+}
