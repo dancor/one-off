1487113ab7d1ccf72f0a4526b452948275fe3378
Daniel P Corson <danl@alum.mit.edu>
2024-06-28 12:27:44 +1000

    cp

--- listDates/d2e/d2e.c
+++ listDates/d2e/d2e.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../../../template/c/err.h"
+#include "../../../template/c/io.h"
 #include "../../../template/c/types.h"
 int curN = 120;
-int readWholeFile(char **c, const char* n){ // raceCondition btwn ftell & fread
-  FILE *f = nue(fopen(n, "r")); nie(fseek(f, 0, SEEK_END));
-  long l = nie(ftell(f)); nie(fseek(f, 0, SEEK_SET)); *c = malloc(l);
-  zie(1 == fread(*c, l, 1, f)); nze(fclose(f)); return l;}
 inline void procDate(const int l, char *c, const int i){
   c[i + 11] = 0;
   // int n = 100 * (c[i + 12] - '0') + 10 * (c[i + 13] - '0') + c[i + 14] - '0'
@@ -29,6 +26,6 @@ readAChar:
   printf("%c", c[i]);
   if (c[i++] == '\n') goto readALine;
   goto readAChar;}
-int main(){
-  char *c; int l = readWholeFile(&c, "/home/danl/etc/plan/todoDanlCal.txt");
+int main(){char *c;
+  int l = readWholeFileOrDie(&c, "/home/danl/etc/plan/todoDanlCal.txt");
   procFile(l, c); free(c);}
