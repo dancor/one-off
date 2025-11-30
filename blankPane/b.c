b9b55731c2f6e62e295c5d397ba4a908501b06fe
. <>
2025-11-30 23:21:03 +0100

    blankPane stop using /template/

--- blankPane/b.c
+++ blankPane/b.c
@@ -1,14 +1,14 @@
 #include <math.h>
 #include <string.h>
-#include "../../template/c/color.h"
-#include "../../template/c/xwin.h"
-#include "../../template/cpp/err.h"
+#include "i.h"
+#include "i/color.h"
+#include "i/xwin.h"
 int main(int argc, char **argv) {
   xwinOn(2560, 1440, (char*)"pbydvz");
   color_t c = (argc >= 2) ? colorRead(argv[1]) : 0;
   for (int i = 0; i < 2560 * 1440; i++) xwinP[i] = c;
   for (;;) {
-    auto *e = zre(xcb_wait_for_event(xwinC));
+    xcb_generic_event_t *e = eoz(xcb_wait_for_event(xwinC));
     if (e->response_type == XCB_EXPOSE) xwin();
   }
 }
